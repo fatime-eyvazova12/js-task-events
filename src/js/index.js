dd59f901038f51827e4a8532927676d600fa59e8
fatima-eyvazova <fatima.eyvazovaa@gmail.com>
2023-09-26 17:19:49 +0400

    added project

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+// const loading = document.querySelector(".loading");
+
+// window.addEventListener("load", function () {
+//   showPage();
+// });
+// function showPage() {
+//   const timeLimit = 5;
+//   let i = 0;
+//   loading.style.opacity = 0;
+//   const timer = setInterval(function () {
+//     i++;
+//     if (i == timeLimit) {
+//       clearInterval(timer);
+//       loading.style.opacity = 1;
+//     }
+//   }, 1000);
+// }
+
+const inputField = document.getElementById("inputField");
+
+inputField.addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    event.preventDefault();
+    const Position = inputField.selectionStart;
+    const BeforeCursor = inputField.value.substring(0, Position - 1);
+    const AfterCursor = inputField.value.substring(Position);
+    inputField.value = BeforeCursor + AfterCursor;
+
+    inputField.setSelectionRange(Position - 1, Position - 1);
+  } else if (event.key === "Backspace") {
+    event.preventDefault();
+    const Position = inputField.selectionStart;
+    inputField.value =
+      inputField.value.substring(0, Position) +
+      " " +
+      inputField.value.substring(Position);
+
+    inputField.setSelectionRange(Position + 1, Position + 1);
+  }
+});
